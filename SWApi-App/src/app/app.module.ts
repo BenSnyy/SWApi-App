e8eb29262d60552fde8e21965e0c581332360a1e
John Whitaker <johnparkswhit@gmail.com>
2019-11-27 11:40:32 -0500

    missed a save

--- SWApi-App/src/app/app.module.ts
+++ SWApi-App/src/app/app.module.ts
@@ -22,9 +22,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-// import { MatSliderModule } from '@angular/material/slider'
-// import { MatSelectModule } from '@angular/material/select'
-// import { MatFormField } from '@angular/material/form-field'
+// .
+
 
 @NgModule({
   declarations: [
