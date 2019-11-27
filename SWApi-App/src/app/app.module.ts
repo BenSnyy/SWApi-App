a1b5ef2355e9809a28367c1f7bedb4de3db0ce17
John Whitaker <johnparkswhit@gmail.com>
2019-11-27 11:38:06 -0500

    css styling

--- SWApi-App/src/app/app.module.ts
+++ SWApi-App/src/app/app.module.ts
@@ -22,6 +22,9 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+// import { MatSliderModule } from '@angular/material/slider'
+// import { MatSelectModule } from '@angular/material/select'
+// import { MatFormField } from '@angular/material/form-field'
 
 @NgModule({
   declarations: [
@@ -49,7 +52,10 @@ import { MatInputModule } from '@angular/material/input';
     NoopAnimationsModule,
     MatMenuModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    // MatSliderModule,
+    // MatSelectModule,
+    // MatFormField
   ],
   providers: [], // no need to place any providers due to the `providedIn` flag...
   bootstrap: [AppComponent]
